b7d8f58a3c53303c57f083ce56440d15b8444f00
omdg1 <omdasgupta36@gmail.com>
2026-09-19 20:58:17 -0700

    QOTD update

--- constants.js
+++ constants.js
@@ -1,1 +1,1 @@
-const qotd = 'If we can find short-term incentives that are consistent with our long-term objectives, it is much easier to make the right decisions in the moment.';
+const qotd = 'What saves a man is to take a step. Then another step. It is always the same step  but you have to take it.';
